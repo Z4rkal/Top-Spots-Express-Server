2c231e65dadb4f0768a5d9d2864d74271967eaf8
Michael Fischer <zarkal@live.com>
2019-06-01 15:46:55 -0700

    Add initial project files

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 // import files and packages up here
-
-
+const express = require('express');
+const data = require('./data.json');
 // create your express server below
-var app;
+var app = express();
+
+app.get('/', function(req,res){
+    console.log('Receiving GET request for: /');
+    res.writeHead(200,{'Content-Type' : 'text/html'});
+    res.end('It\'s working :)');
+});
+
+app.get('/data', function(req,res){
+    console.log('Receiving GET request for: /data');
+    res.json(data);
+});
 
 // add your routes and middleware below
 
